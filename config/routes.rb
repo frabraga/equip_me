39f037347712473abb72894c76f30cf41d40406c
Francisco Braga <franciscobragafernandes@gmail.com>
2018-08-14 14:33:57 -0300

    Add pundit basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'equipments#index'
+
   resources :bookings do
-    resources :reviews, only [ :new, :create ]
+    resources :reviews, only: [ :new, :create ]
   end
 
 
