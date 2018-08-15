7d62c14d74f694ca142325c89faeb01f602ffa24
Ales Sinkovec <ales@parity.solutions>
2018-08-15 13:39:08 -0300

    fix seed, style form

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,34 +17,51 @@ puts "adding all fucks"
   user.save!
 end
 
-100.times do
-  test_num = rand(1..100)
+photos = [
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344769/a7hattlipiddwumpfb24.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344770/kdxeuqi4tcn2hc0osh3e.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344773/kadlhcphio3ahb37dchj.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344765/xymyzcskkwxzgyyox4lt.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344771/r8alcq7cuo6q6bw43uk0.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344767/thkyz9qla9crse8owrvz.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344764/fyodsdxat0xxlho0khvd.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344761/bm26blws9hzp2m4vtcde.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344754/pvxgkbqzfhf14gplcke1.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344759/k3gqyzdl5uzrxmaogqrg.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344740/epgflc09ibmkokphv1g8.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344739/jz5jzup3wp2ws5cebp4u.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344735/d1agib7oiwpd8kjgpjid.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344746/bpopov3xzaidk77myo3q.jpg",
+"https://res.cloudinary.com/djjncf5oe/image/upload/v1534344731/mgxhkie9neavspdjvm5k.jpg"
+]
+
+10.times do
   equipment = Equipment.new(
     name: Faker::Pokemon.move,
     description: Faker::StarWars.quote,
     price: rand(50..500),
     user_id: rand(1..10),
     category: Faker::Esport.game,
-    remote_photo_url: "https://picsum.photos/300/175/?image=#{test_num}"
+    remote_photo_url: photos.sample
   )
   equipment.save!
 end
 
-30.times do
+15.times do
   booking = Booking.new(
     user_id: rand(1..10),
-    equipment_id: rand(1..100),
+    equipment_id: rand(1..10),
     start_date: Faker::Date.between(2.days.ago, Date.today),
     end_date: Faker::Date.forward(rand(5..23))
   )
   booking.save!
 end
 
-50.times do
+30.times do
   review = Review.new(
     description: Faker::StarWars.wookiee_sentence,
     rating: rand(0..5),
-    booking_id: rand(1..30)
+    booking_id: rand(1..15)
   )
 end
 
