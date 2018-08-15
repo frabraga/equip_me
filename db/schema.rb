c317cda398dc499bbedfff372c9802d49b641dfc
Francisco Braga <franciscobragafernandes@gmail.com>
2018-08-15 11:03:48 -0300

    Add category field to equipment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_193051) do
+ActiveRecord::Schema.define(version: 2018_08_15_135818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_08_14_193051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "category"
     t.index ["user_id"], name: "index_equipment_on_user_id"
   end
 
