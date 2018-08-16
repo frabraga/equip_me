87c84587a9f76a31b1ebbc3e62f30d46364cf448
Adam Sinning <asinning25@gmail.com>
2018-08-16 11:40:00 -0300

    destroy feature created for equipment

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -40,6 +40,9 @@ class EquipmentsController < ApplicationController
   end
 
   def destroy
+    authorize @equipment
+    @equipment.destroy
+    redirect_to list_equipments_path
   end
 
   private
